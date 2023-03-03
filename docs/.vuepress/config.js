e0aaf3dd6a27811a2b53aa238fbd5fac5e4d71c1
冯琦昌 <2309997549@qq.com>
2023-03-03 22:16:28 +0800

    执行脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,14 +14,14 @@ module.exports = {
 			{
 				title: '列表', // 必要的
 				collapsable: false, // 可选的, 默认值是 true,
-				children:[encodeURI('/常用的正则表达式大全')],
-			},
+				children,
+			}
 			// {
 			// 	title: 'vue2', // 必要的
 			// 	// path: '/vue2/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
 			// 	collapsable: false, // 可选的, 默认值是 true,
 			// 	children:[]
 			// }
-		],
-	},
+		]
+	}
 }
